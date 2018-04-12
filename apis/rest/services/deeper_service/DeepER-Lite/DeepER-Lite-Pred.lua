95cbd5090e6fe215e163a7baa6cf9869c84ab56f
Essam Mansour <essam.mansour@gmail.com>
2018-04-12 17:04:05 +0300

    add new Deeper

--- apis/rest/services/deeper_service/DeepER-Lite/DeepER-Lite-Pred.lua
+++ apis/rest/services/deeper_service/DeepER-Lite/DeepER-Lite-Pred.lua
@@ -1,11 +1,12 @@
 local sys = require 'sys'
 local csvigo = require 'csvigo'
+local threads = require 'threads'
+
 require 'torch'
 require 'nn'
 require 'xlua'
 require 'optim'
-require 'dp'
---require 'cunn'
+--require 'dp'
 
 local total_time = sys.clock()
 
@@ -30,6 +31,7 @@ cmd:option('-save', '', 'subdirectory to save/log experiments in')
 cmd:option('-type', 'float', 'type: double | float | cuda')
 cmd:option('-simMeasure', '', 'cosineDiff | diff | cosineDiff')
 cmd:option('-threshold_file_path','' , 'save path to the negative drop threshold')
+cmd:option('-empty_cosine_penalty','0' , 'Empty Cosine Penalty')
 cmd:option('-computeFeatures', '', 'recompute features anew do not load from saved yes | no')
 
 cmd:text()
@@ -40,6 +42,8 @@ opt.threshold = threshold_file:readFloat()
 threshold_file:close()
 
 print(opt)
+
+torch.setdefaulttensortype('torch.FloatTensor')
 
 if opt.type == 'float' then
    print('==> switching to floats')
@@ -65,6 +69,17 @@ local function split(input, sep)
 end
 
 
+
+
+local nthread = 8
+local njob = nthread
+
+local function TableLength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
 local function FillDataTable(pairsTable, firstTable, secondTable, indextoIdTable, indextoIdTableCounter)
   local pairsDataTable = {}
   for i=1, #pairsTable do
@@ -83,6 +98,15 @@ end
 
 local cosine = nn.CosineDistance()
 
+local all_case_counter = 0
+local here_cos_1 = 0
+local here_cos_2 = 0
+local here_cos_3 = 0
+
+local here_diff_1 = 0
+local here_diff_2 = 0
+local here_diff_3 = 0
+
 local function ExtractFeatures(dataTable, negativeSamplingThreshold, emtpyCosinePenalty)
   local firstFeaturesTensor = torch.FloatTensor(opt.numTableFields,opt.embeddingSize)
   local secondFeaturesTensor = torch.FloatTensor(opt.numTableFields,opt.embeddingSize)
@@ -99,27 +123,28 @@ local function ExtractFeatures(dataTable, negativeSamplingThreshold, emtpyCosine
   local included = torch.Tensor(dataTensor:size()[1])
   local numIncluded = 0
   for i = 1, #dataTable do
-    xlua.progress(i, #dataTable)
+    all_case_counter = all_case_counter + 1
+    xlua.progress(i,#dataTable)
     for j=1, opt.numTableFields do
       firstFeaturesTensor:zero()
       secondFeaturesTensor:zero()
       local sentence1 = dataTable[i][1][j]
-      local numWordsInSentences = 0
+      local numWordsInSentences_1 = 0
       for word in sentence1:gmatch("%w+") do --TODO: very naive to break on white space only
         firstFeaturesTensor[j]:add(word2vec:word2vec(word))
-        numWordsInSentences = numWordsInSentences + 1
+        numWordsInSentences_1 = numWordsInSentences_1 + 1
       end
-      firstFeaturesTensor[j]:div(numWordsInSentences)
+      firstFeaturesTensor[j]:div(numWordsInSentences_1)
 
       local sentence2 = dataTable[i][2][j]
       
       
-      numWordsInSentences = 0
+      local numWordsInSentences_2 = 0
       for word in sentence2:gmatch("%w+") do --TODO: very naive to break on white space only, use porter or something more advanced
         secondFeaturesTensor[j]:add(word2vec:word2vec(word))
-        numWordsInSentences = numWordsInSentences + 1
+        numWordsInSentences_2 = numWordsInSentences_2 + 1
       end
-      secondFeaturesTensor[j]:div(numWordsInSentences)
+      secondFeaturesTensor[j]:div(numWordsInSentences_2)
 
       local dist = cosine:forward{firstFeaturesTensor[j],secondFeaturesTensor[j]}
 
@@ -127,24 +152,30 @@ local function ExtractFeatures(dataTable, negativeSamplingThreshold, emtpyCosine
       local diff = nn.Abs()(firstFeaturesTensor[j] - secondFeaturesTensor[j])
       diff = torch.norm(diff)
       if dist[1]~=dist[1] then  --cosine is NaN
-        if string.len(sentence1) == 0 and string.len(sentence2) == 0 then
+        if sentence1=="" and sentence2 =="" then
+          here_cos_1 = here_cos_1 + 1
           dist = 1
-        elseif (string.len(sentence1) == 0 and string.len(sentence2) < 3) or (string.len(sentence1) < 3 and string.len(sentence2) == 0) then
-          dist = 1
-        else
+        elseif sentence1~="" and sentence2 =="" then 
+          here_cos_2 = here_cos_2 + 1
+          dist = emtpyCosinePenalty
+        elseif sentence1=="" and sentence2 ~="" then
+          here_cos_3 = here_cos_3 + 1
           dist = emtpyCosinePenalty  -- cosine is NaN, so assigning worst value from a cosine similarity point of view
         end
       end  
 
       if diff~=diff then  --diff is NaN, this is mostly due to a mising attribute(sentence)
-        if string.len(sentence1) == 0 and string.len(sentence2) == 0 then
+        if sentence1=="" and sentence2 =="" then
+          here_diff_1 = here_diff_1 + 1
           diff = 0
         elseif sentence1~="" and sentence2 =="" then 
+          here_diff_2 = here_diff_2 + 1
           diff = torch.norm(firstFeaturesTensor[j])
-          diff = diff / string.len(sentence1)
+          diff = diff / numWordsInSentences_1
         elseif sentence1=="" and sentence2 ~="" then
+          here_diff_3 = here_diff_3 + 1
           diff = torch.norm(secondFeaturesTensor[j])
-          diff = diff / string.len(sentence2)
+          diff = diff / numWordsInSentences_2
         end
       end
 
@@ -158,7 +189,7 @@ local function ExtractFeatures(dataTable, negativeSamplingThreshold, emtpyCosine
      end
    end
    
-   if dataTensor[ {{i},{1,4} } ]:mean() < negativeSamplingThreshold then
+   if dataTensor[ {{i},{1,4} } ]:mean() <= negativeSamplingThreshold then
     included[i] = 0
    else
     included[i] = 1
@@ -202,25 +233,26 @@ if opt.computeFeatures == 'yes' then --   not paths.filep(opt.positivePairsTrain
   local firstDataTable = {}
   local secondDataTable = {}
 
-  for i=1,#firstData do
+  for i=2,#firstData do
     local key
     local value = {}
     key = firstData[i][1]
     for j=2,#firstData[i] do
       table.insert(value,firstData[i][j])
     end
-    firstDataTable[key] = value
+    firstDataTable[key] = value  
   end
 
-  for i=1,#secondData do
+  for i=2,#secondData do
     local key
     local value = {}
     key = secondData[i][1]
     for j=2,#secondData[i] do
       table.insert(value,secondData[i][j])
     end
-    secondDataTable[key] = value
+    secondDataTable[key] = value    
   end
+  
   t = sys.toc()    
   print("Data Preprocessing took: " .. t .. " seconds\n")
   
@@ -229,7 +261,7 @@ if opt.computeFeatures == 'yes' then --   not paths.filep(opt.positivePairsTrain
   
 
   print("Memory before FillDataTable: " .. collectgarbage("count")/1000000 .. " GB")
-  local predPairsTable = FillDataTable(allPairs, firstDataTable, secondDataTable,predIndexToTable,1)
+  local predPairsTable = FillDataTable(allPairs, firstDataTable, secondDataTable, predIndexToTable,1)
   firstDataTable = nil
   secondDataTable =nil
   print("Memory after FillDataTable before collect: " .. collectgarbage("count")/1000000 .. " GB")
@@ -241,7 +273,7 @@ if opt.computeFeatures == 'yes' then --   not paths.filep(opt.positivePairsTrain
   print("Extracting Features from Data Table ...")
   sys.tic()
 
-  predTensorFull,included, numIncluded = ExtractFeatures(predPairsTable, opt.threshold,-1)
+  predTensorFull,included, numIncluded = ExtractFeatures(predPairsTable, opt.threshold,opt.empty_cosine_penalty)
   
   t = sys.toc()
   print("Extracting Features from Data Table took: " .. t .."seconds\n")
@@ -258,14 +290,16 @@ if opt.computeFeatures == 'yes' then --   not paths.filep(opt.positivePairsTrain
   sys.tic()
   print("Filtering by cosine threshold: " .. opt.threshold)
   predTensor = torch.Tensor(numIncluded, predTensorFull:size()[2])
+  --reducedPredIndexToTable = {}
   local predCounter = 0
   for i = 1, predTensorFull:size()[1] do
     if included[i] == 1 then
-        predCounter = predCounter + 1
+      predCounter = predCounter + 1
       predTensor[predCounter] = predTensorFull[i]
+      predIndexToTable[predCounter] = predIndexToTable[i]
     end
   end
-  
+  --predIndexToTable = reducedPredIndexToTable
 
   print('size after cosine sim thresholding: ' .. numIncluded)  
   print('size after cosine sim thresholding: (check): ' .. predTensor:size()[1])
@@ -307,17 +341,20 @@ local function evaluate(data, model, is_dev)
    for t = 1,data:size() do
       xlua.progress(t, data:size())
       local input = data.data[t]
-      
-      -- if opt.type == 'double' then input = input:double()
-      -- elseif opt.type == 'float' then input = input:float()
-      -- elseif opt.type == 'cuda' then input = input:cuda() end
-      
+      if opt.type == 'double' then input = input:double()
+      elseif opt.type == 'cuda' then input = input:cuda() end
       local pred = model:forward(input)
       if pred[1] > pred[2] then
         label = 1
       else
         label = 2
       end
+      
+      -- print(predIndexToTable[t])
+      -- print(input)
+      -- print(pred)
+      -- print(label)
+
       table.insert(test_predictions, {predIndexToTable[t], label})  
    end
    time = sys.clock() - time
@@ -329,9 +366,6 @@ end
 local best_dev_model_file_path = paths.concat(opt.save, 'best_dev_model.net') 
 best_dev_model = torch.load(best_dev_model_file_path)
 
-if opt.type == 'cuda' then
-   best_dev_model:cuda()
-end
 
 local test_p, test_r, test_f1, test_confusion = evaluate(predData, best_dev_model, false)
 
@@ -339,10 +373,16 @@ local count_matches = 0
 for k,v in ipairs(test_predictions) do
   if v[2] == 1 then
     count_matches = count_matches + 1
-    test_predictions_file:writeString(v[1][1]  .. ','  .. v[1][2] ..'\n')
+    test_predictions_file:writeString('\"' .. v[1][1]  .. '\",'  .. '\"' .. v[1][2] .. '\"' ..'\n')
   end
+
 end
+
 test_predictions_file:close()
+
+print('all_case_counter ' .. all_case_counter)
+
+
 total_time = sys.clock() - total_time
 print("\n==> Total time = " .. (total_time) .. ' seconds, predicted: ' .. (predData:size()) .. ' pairs\n' )
 print("(" .. count_matches .. ")".. " MATCHES FOUND\n")
