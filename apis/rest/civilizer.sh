4287381a89f0cf7a6c2a0b9b72db475b10c6c613
Michael Houle <michael.houle@ll.mit.edu>
2018-08-13 13:35:06 -0400

    Modified apis/Dockerfile to call civilizer.sh and build GloVe.t7 if not present Added .gitignore files to keep empty directories in repo

---
+++ apis/rest/civilizer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ./rest/services/deeper_service/DeepER-Lite/glove/
+if [ ! -f GloVe.t7 ]
+then
+    echo GloVe word vector file not found.  Initializing...
+    glove=glove.840B.300d.zip
+    echo Step 1/5 - Downloading $glove...
+    wget http://nlp.stanford.edu/data/$glove
+    echo Step 2/5 - Unzipping $glove...
+    unzip $glove > /dev/null
+    echo Step 3/5 - Deleting $glove...
+    rm -f $glove
+    sed "/outfilename =/ s/'[^']*'/'\/app\/storage\/glove\/GloVe.t7'/" -i glove_setup.lua
+    [ ! -d /app/storage/glove ] && mkdir /app/storage/glove
+    echo Step 4/5 - Converting GloVe words to vectors...
+    th glove_setup.lua > /dev/null
+    echo Step 5/5 - Deleting ${glove/zip/txt}...
+    rm -f ${glove/zip/txt}
+    echo GloVe initialization completing.
+fi
+cd /app/
+
+python ./rest/civilizer.py
