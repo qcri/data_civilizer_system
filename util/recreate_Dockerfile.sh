08034e3f42f337c31e1415c09cdf6f7a76f36a99
Michael Houle <michael.houle@ll.mit.edu>
2018-11-13 13:33:53 -0500

    Improve FROM directive determination

--- util/recreate_Dockerfile.sh
+++ util/recreate_Dockerfile.sh
@@ -8,8 +8,8 @@ then
     exit 1
 fi
 
-from=$(docker inspect -f "{{ json .RepoTags }}" $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed 's/^[^"]*"\|".*$//g')
-echo "FROM $from"
+#from=$(docker inspect -f "{{ json .RepoTags }}" $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed 's/^[^"]*"\|".*$//g')
+#echo "FROM $from"
 
 docker history --no-trunc $1 |
 sed -r 's/^([^ ]+) +(CREATED|.+ ago) +(CREATED BY|\/bin.+[^ ]) +(SIZE|[0-9.]+ [kMG]?B) +(.*)?$/\1\t\2\t\3\t\4\t\5/' |
@@ -17,6 +17,7 @@ tail -n +2 |
 tac |
 sed -r '1,/^[^<]/ s/^([^\t]*\t[^\t]*\t)/\1#/' |
 cut -f3 |
+cat <(echo "FROM $(docker inspect -f '{{ .RepoTags }}' $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed 's/[][]//g')") - |
 sed -r 's/^(#?)\/bin\/sh -c/\1RUN/' |
 sed -r 's/^(#?)RUN #\(nop\) (ADD|COPY)/#\2/' |
 sed -r 's/^(#?)RUN #\(nop\)  ?(CMD|ENTRYPOINT|ENV|EXPOSE|LABEL|MAINTAINER|USER|WORKDIR)/\1\2/' |
@@ -28,3 +29,9 @@ sed '/^#RUN/ s/  &&/ \\\n# \&\&/g' |
 sed '/^RUN/ s/     / \\\n    /g' |
 sed '/^RUN/ s/  &&/ \\\n \&\&/g' |
 sed 's/   *\\$/ \\/'
+
+#cat <(docker inspect -f "{{ json .RepoTags }}" $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed -e 's/^[^"]*"/FROM /' -e 's/".*$//') - |
+#cat <(docker inspect -f "{{ .RepoTags }}" $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed -e 's/^./FROM /' -e 's/.$//') - |
+#cat <(echo -n "FROM ") <(docker inspect -f "{{ .RepoTags }}" $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed 's/[][]//g') - |
+#cat <(docker inspect -f "{{ .RepoTags }}" $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed -e 's/[][]//g' -e 's/^/FROM /') - |
+#cat <(docker inspect -f "{{ .RepoTags }}" $(docker history $1 | sed 's/ .*$//;/<missing>/!h;g;$!d') | sed 's/[][]//g' | sed 's/^/FROM /') - |
