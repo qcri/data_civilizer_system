e6227915ffba3edb19b3792f6d1c487730c9ef84
Giovanni Simonini <giovanni.simonini@gmail.com>
2018-12-17 22:34:00 +0300

    fixed parameter passing for blocking key in civilizer.py

--- apis/rest/civilizer.py
+++ apis/rest/civilizer.py
@@ -177,23 +177,23 @@ def executeOperator(operator):
     elif (class_name == "civilizer.basic.operators.EntityMatching-DeepER-Train"):
         print("DataCleaning-DeepER-Train")
         params = {
-            "dataset_folder_path":operator["parameters"]["param2"],
-            "ltable_file_name":operator["parameters"]["param4"],
-            "rtable_file_name":operator["parameters"]["param5"],
-            "labeled_file":operator["parameters"]["param6"],
-            "lblocking_key":"",
-            "rblocking_key":""
+            "metadata_path":operator["parameters"]["param2"],
+            "ltable_file_path":operator["parameters"]["param4"],
+            "rtable_file_path":operator["parameters"]["param5"],
+            "labeled_file_path":operator["parameters"]["param6"]
         }
         deeper_lite_api.executeServiceTrain(params)
 
     elif (class_name == "civilizer.basic.operators.EntityMatching-DeepER-Predict"):
         print("DataCleaning-DeepER-Predict")
         params = {
-                "dataset_folder_path":operator["parameters"]["param2"],
+                "metadata_path":operator["parameters"]["param2"],
                 "out_file_path":operator["parameters"]["param3"],
-                "ltable_file_name":operator["parameters"]["param4"],
-                "rtable_file_name":operator["parameters"]["param5"],
-                "candidates_file":operator["parameters"]["param6"]
+                "ltable_file_path":operator["parameters"]["param4"],
+                "rtable_file_path":operator["parameters"]["param5"],
+                "candidates_file_path":operator["parameters"]["param6"],
+                "lblocking_key":operator["parameters"]["param7"],
+                "rblocking_key":operator["parameters"]["param7"]
         }
         deeper_lite_api.executeServicePredict(params)
 
