5334d3f74f4a32a7fd0e29c04a45700f5173f306
Michael Houle <michael.houle@ll.mit.edu>
2018-12-20 11:32:15 -0500

    Improved error trapping on proxied calls to apis

--- studio/controllers/operators.js
+++ studio/controllers/operators.js
@@ -6,16 +6,18 @@ function Operator() {
 }
 
 Operator.getByType = function(req, res){
-    console.log(req.method + " " + process.env.API_SERVER_URL + "/rheem" + req.url + " @ " + new Date().toISOString());
+     var target = req.method + " " + process.env.API_SERVER_URL + "/rheem" + req.url;
+    console.log(target + " @ " + new Date().toISOString());
     // console.log(req.getallheaders);
     // console.log(JSON.stringify(req.headers, null, 4));
 
-    try {
-        proxy.forwardRequest(req, res);
-    }
-    catch(e) {
-        return res.json([])
-    }
+    // Catch proxy errors if the backend is down...
+    proxy.proxy.on('error', function (err, req, res) {
+        console.log("Proxy error on " + target + "\n" + JSON.stringify(err, null, 4));
+        return res.end();
+    });
+
+    proxy.forwardRequest(req, res);
 }
 
 module.exports = Operator;
