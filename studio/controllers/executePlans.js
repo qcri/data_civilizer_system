76a7f5e611cc4c6df433a509e266f0f9c225b885
Michael Houle <michael.houle@ll.mit.edu>
2019-01-17 09:13:31 -0500

    Updated to support executing operators on and quering advanced progress status from multiple backend

--- studio/controllers/executePlans.js
+++ studio/controllers/executePlans.js
@@ -9,15 +9,17 @@ request = require('request-json');
 const uuid = require('uuid');
 
 var client = request.createClient(process.env.API_SERVER_URL);
+var clients = {};
 
 // Independent execution engine for gemPlans -- not yet implemented
 
 const STATE_WAITING = 0;
-const STATE_ACTIVE  = 1;
-const STATE_USER    = 2;
-const STATE_LOOP    = 3;
-const STATE_DONE    = 4;
-const STATE_ERROR   = 5;
+const STATE_BLOCKED = 1;
+const STATE_ACTIVE  = 2;
+const STATE_USER    = 3;
+const STATE_LOOP    = 4;
+const STATE_DONE    = 5;
+const STATE_ERROR   = 6;
 
 var planStatus = {};
 
@@ -32,14 +34,15 @@ function ExecutePlans_rheem(rheemPlan) {
 
   // Assign a unique id for this plan execution
   // Create a status object to track and report plan execution
-//var run_id = ("0000000" + Math.floor(Math.random() * 2147483647).toString(16)).slice(-8);
   var run_id = uuid.v1();
   var ops = {};
   console.log("run_id: " + run_id);
   var status = planStatus[run_id] = {
     "run_id": run_id,
     "state": STATE_ACTIVE,
-    "op_status": ops
+    "op_status": ops,
+    "serial": {},
+    "blocked": {}
   };
 
   console.log("");
@@ -87,10 +90,10 @@ function ExecutePlans_rheem(rheemPlan) {
 
   function executePlan(keys) {
     console.log("executePlan(" + keys + ") - Looking for operators to execute");
-    for(var key of keys) {
+    for(var key of Object.keys(status.blocked).concat(keys)) {
       var op = ops[key];
       console.log("Checking op: " + op.name);
-      if(op.state == STATE_WAITING) {
+      if(op.state < STATE_ACTIVE) {
         var waiting = op.depends_on.length;
         for(var name of op.depends_on) {
           if(ops[name].state == STATE_DONE) {
@@ -98,8 +101,19 @@ function ExecutePlans_rheem(rheemPlan) {
           }
         }
         if(!waiting) {
-          executeNode(op);
-          // return; // uncomment to serialize execution
+          // New default is to serialize execution of instances of the same
+          // operator unless they explicitly specify that they support
+          // parallelization.
+          if(!(op.java_class in status.serial) || (op.features && op.features.parallel)) {
+            if(op.name in status.blocked) {
+              delete status.blocked[op.name];
+            }
+            executeNode(op);
+          } else {
+            status.blocked[op.name] = true;
+            op.state = STATE_BLOCKED;
+          }
+          // return; // uncomment to serialize plan execution
         }
       }
     }
@@ -119,12 +133,22 @@ function ExecutePlans_rheem(rheemPlan) {
 
   function executeNode(op) {
     active++;
+    if(!op.features || !op.features.parallel) {
+      status.serial[op.java_class] = op.name;
+    }
     console.log("Executing: " + op.name);
     op.state = STATE_ACTIVE;
-    var url = "http://apis:8089/rheem/plan_exec_op";
+    var url = op.baseurl + "/rheem/plan_exec_op";
+    console.log("POST " + url + " @ " + new Date().toISOString());
+    if(!(op.baseurl in clients)) {
+      clients[op.baseurl] = request.createClient(op.baseurl);
+    }
     console.log("Sending: " + JSON.stringify(op, null, 4));
-    client.post(url, op, function(err, data) {
+    clients[op.baseurl].post(url, op, function(err, data) {
       active--;
+      if((op.java_class in status.serial) && (status.serial[op.java_class] == op.name)) {
+        delete status.serial[op.java_class];
+      }
       var next_keys = [];
       if(err) {
         // HTTP errors connecting to opertor service
@@ -224,8 +248,102 @@ router.get('/latest_plan_execution', function(req, res) {
 
 router.get('/progress', function(req, res) {
   if(req.query.run_id in planStatus) {
-    console.log("GET /progress?run_id=" + req.query.run_id);
-    return res.json(planStatus[req.query.run_id]);
+    var run_id = req.query.run_id;
+    console.log("GET /progress?run_id=" + run_id);
+
+if(0) {
+    //
+    // Old method -
+    // -- optimized to make a single progress call to the backend
+    //    apis container by specifying mulitple "name=" variables
+    //    in the query string.
+    // -- does not support operators coming from multiple backends,
+    //    which would be useful to support future operators running
+    //    in separte containers
+    //
+
+    var names = "";
+    var op_status = planStatus[run_id].op_status;
+    for(var name in op_status) {
+      if(op_status[name].state == STATE_ACTIVE) {
+        console.log("op_status[" + name + "]:");
+        console.log(op_status[name]);
+        if(op_status[name].features && op_status[name].features.progress) {
+          names += "&name=" + name;
+        }
+      }
+    }
+
+    if(names) {
+      console.log("GET " + process.env.API_SERVER_URL + "/rheem/progress?run_id=" + run_id + names + " @ " + new Date().toISOString());
+      client.get('/rheem/progress?run_id='+ run_id + names, function(err, reshttp, body) {
+        console.log(body);
+        for(var name in body) {
+          if(body[name].progress) {
+            op_status[name].progress = body[name].progress;
+          }
+        }
+        return res.json(planStatus[run_id]);
+      });
+    } else {
+      return res.json(planStatus[run_id]);
+    }
+
+} else {
+
+    //
+    // New method -
+    // -- Groups operators that support advanced progress reporting
+    //    by 'baseurl' property (which identifies the api container
+    //    of the operator)
+    // -- Makes one call per backend container and aggregates results
+    //
+
+    var names = {};
+    var url_cnt = 0;
+    var op_status = planStatus[run_id].op_status;
+    for(var name in op_status) {
+      if(op_status[name].state == STATE_ACTIVE) {
+        console.log("op_status[" + name + "]:");
+        console.log(op_status[name]);
+        if(op_status[name].features && op_status[name].features.progress) {
+          var baseurl = op_status[name].baseurl;
+          if(!(baseurl in names)) {
+            names[baseurl] = "";
+            url_cnt++;
+          }
+          names[baseurl] += "&name=" + name;
+        }
+      }
+    }
+
+    if(url_cnt) {
+      for(var baseurl in names) {
+        console.log("GET " + baseurl + "/rheem/progress?run_id=" + run_id + names[baseurl] + " @ " + new Date().toISOString());
+        if(!(baseurl in clients)) {
+          clients[baseurl] = request.createClient(baseurl);
+        }
+        clients[baseurl].get('/rheem/progress?run_id='+ run_id + names[baseurl], function(err, reshttp, body) {
+          console.log(body);
+          if(body) {
+            for(var name in body) {
+              if(body[name].progress) {
+                op_status[name].progress = body[name].progress;
+              }
+            }
+          }
+          if(--url_cnt == 0) {
+            return res.json(planStatus[run_id]);
+          }
+        });
+      }
+    } else {
+      return res.json(planStatus[run_id]);
+    }
+
+}
+
+    return;
   }
 
   console.log("GET " + process.env.API_SERVER_URL + "/rheem/progress?run_id=" + req.query.run_id + " @ " + new Date().toISOString());
